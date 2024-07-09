f9f82da21c2ce42c561fb0fe15ef14a8fa390169
BigHeadliner <bigheadliner@gmail.com>
2024-07-09 22:25:20 +0300

    started work on video section

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,34 @@
-$(function () {   
-     
-    
- 
 
-  
- 
-  
- 
-  
-  
-  
-  
-     
-
- 
+$(function () {
 
 
 
-  
+    window.addEventListener('scroll', function () {
+        const header = document.querySelector('.header__top');
+        if (window.scrollY > 0) {
+            header.style.opacity = '1';
+          } else {
+              header.style.opacity = '0.8';
+          }
+    });
 
 
-  
-  
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
